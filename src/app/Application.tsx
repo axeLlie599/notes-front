41711cbe5b07945a31a70a3d09b0a3fbfdaaceb8
axeLlie599 <sogachevsaho@gmail.com>
2024-11-03 15:56:53 +0300

    the template

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -1,48 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "./styles/main.css";
 import "./styles/material-icons/material-icons.css";
 
 import useTheme from "./hooks/theme.app";
-import Header from "./components/general/header/Header";
-import IconButton from "./components/widgets/buttons/icon-button/IconButton";
-import FAB from "./components/widgets/buttons/fab/FAB";
-import Main from "./components/general/main/Main";
-import useNotes from "./hooks/notes.api";
 
 export default function Application() {
   const ENABLE_THEME_CHANGE = true;
-  const themeHook = useTheme({ enabled: ENABLE_THEME_CHANGE });
-  const notesHook = useNotes("https://notes-api-qvr8.onrender.com");
+  //const themeHook = useTheme({ enabled: ENABLE_THEME_CHANGE });
+  useTheme({ enabled: ENABLE_THEME_CHANGE });
 
   return (
-    <div className="Application wide-screen">
-      <Header blur fixed>
-        <Header.SectionLeft>
-          <span id="app_title">{document.title}</span>
-        </Header.SectionLeft>
-        <Header.SectionRight>
-          <IconButton
-            icon="clear_all"
-            tooltipLabel="Clear all"
-            onClick={notesHook.clearNotes}
-            disabled={
-              notesHook.error || notesHook.notes.length === 0 ? true : false
-            }
-          />
-          <IconButton
-            icon={themeHook.isDark ? "light_mode" : "dark_mode"}
-            onClick={themeHook.toggle}
-            tooltipLabel={`Toggle theme (${themeHook.currentTheme})`}
-          />
-        </Header.SectionRight>
-      </Header>
-      <Main hook={notesHook} />
-      <FAB
-        icon="add"
-        pos={FAB.Position.RightBottom}
-        label="Add note"
-        onClick={() => notesHook.createNote()}
-        disabled={notesHook.error ? true : false}
-      />
+    <div className="Application wide-screen flex-center">
+      <code>Now you can write this from zero</code>
     </div>
   );
 }
